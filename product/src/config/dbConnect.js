cc2849280c054ad60d80237b95c03d7f752e0e78
Cairo Ribeiro <cairo.gr@live.com>
2023-02-01 16:49:51 -0300

    fix: fix in connection with mongodb

--- product/src/config/dbConnect.js
+++ product/src/config/dbConnect.js
@@ -6,27 +6,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/ecomm");
 let db = mongoose.connection;
 
 export default db;
-
-
-
-
-
-
-
-
-
-
-// const connectToDatabase = () => {
-//     mongoose.set('strictQuery',false);
-//     mongoose.connect('mongodb://127.0.0.1:27017/ecomm',{useNewUrlParser: true}, 
-//     (err) => {
-//             if (err){
-//                 return console.log("Error:", err)
-//             }
-//             return console.log("BD conectado com sucesso!")
-//         }
-// )}
-
-// mongoose.se
-
-// export { connectToDatabase };
