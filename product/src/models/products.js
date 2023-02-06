fbdcbe7342968f3ac81756cb58ee3093c46b7461
Cairo Ribeiro <cairo.gr@live.com>
2023-02-06 16:50:22 -0300

    feat: created validations for product registration

--- product/src/models/products.js
+++ product/src/models/products.js
@@ -3,12 +3,24 @@ import mongoose from "mongoose";
 const productsSchema = new mongoose.Schema(
   {
     id: {type: Number},
-    name: {type: String, required: true, minLength: 4},
-    slug: {type: String},
+    name: {type: String, required: true, minLength: 4, validate: {
+      validator: function(v) {
+      return /^([A-Za-z]{1})/.test(v);
+      }, message: props => `${props.value} is not a valid name!`}},
+    description: { type: String, requie: true},
+    slug: {type: String, validate: {
+      validator: function(v) {
+      return /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(v);
+      }, message: props => `${props.value} is not a valid name!`}},
+      
     unitPrice: {type: Number, min: 1},
     quantityStock: {type: Number, min: 1, max: 10000},
     idCategory: {type: mongoose.Schema.Types.ObjectId, ref: 'categories', required: true},
 
+
+  },
+  {
+    versionKey: false
   }
 );
 
