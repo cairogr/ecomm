3ed5c40bec2a7b15ac16b112ad540853acc64359
Cairo Ribeiro <cairo.gr@live.com>
2023-02-17 16:01:00 -0300

    created finance service

---
+++ order/src/main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const routes = require("../src/routes");
+
+const app = express();
+console.log("Init Service Orders");
+
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`);
+});
+routes(app);
+
+module.exports = app;
---
+++ order/src/routes/index.js
@@ -0,0 +1,7 @@
+const bodyParser = require("body-parser");
+const orders = require("./ordersRoute.js");
+
+module.exports = (app) => {
+  app.use(bodyParser.json());
+  app.use(orders);
+};
