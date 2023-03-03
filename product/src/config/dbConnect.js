923cc9e24d4bfd6fea968b68f78ee13b7fb52601
Cairo Ribeiro <cairo.gr@live.com>
2023-03-03 10:31:07 -0300

    fix: configured dotenv to run tests concurrently with the service in docker

--- product/src/config/dbConnect.js
+++ product/src/config/dbConnect.js
@@ -1,8 +1,15 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+  
 import mongoose from "mongoose";
+
+const DB_HOST = process.env.NODE_ENV === "test" ? "127.0.0.1" : process.env.DB_HOST;
+
+console.log(DB_HOST);
 
 mongoose.set("strictQuery", false);
 mongoose.connect(
-	"mongodb://admin:secret@mongo-ecomm:27017/ecomm?authSource=admin"
+	`mongodb://admin:secret@${DB_HOST}:27017/ecomm?authSource=admin`
 );
 
 let db = mongoose.connection;
