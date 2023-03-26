1e921c912f434e6d28cdb85840439fce335aa237
Cairo Ribeiro <cairo.gr@live.com>
2023-03-26 01:33:45 -0300

    feat: enable limit request

--- api-gateway/src/routes/routerLogin.js
+++ api-gateway/src/routes/routerLogin.js
@@ -2,12 +2,19 @@ import express from 'express';
 import {authLocal} from'../middlewares/auth.js';
 import ApiGatewayController from'../controller/ApiGatewayController.js';
 import { createProxyMiddleware } from "http-proxy-middleware";
+import * as rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 20 // 20 requests
+});
+
 
 const router = express.Router();
 
 router
 	.get('/', (req, res)=>{res.status(200).send({ titulo: 'Ecomm' });})
-	.post('/login', authLocal, ApiGatewayController.login)
+	.post('/login', authLocal, limiter, ApiGatewayController.login)
 
 export default router
 
